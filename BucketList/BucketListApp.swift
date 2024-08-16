fcb8bb6b71c2f199eebb11ab43018229732ebddf
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-08-16 11:09:25 -0400

    Initial commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Rodrigo on 16-08-24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
