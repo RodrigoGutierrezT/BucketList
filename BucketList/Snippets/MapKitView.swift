f4c075e72555ac8a0f0504c3d4dc47534b3a0f4e
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-08-17 00:01:05 -0400

    feat: Add MapKitView snippet

---
+++ BucketList/Snippets/MapKitView.swift
@@ -0,0 +1,52 @@
+//
+//  MapKitView.swift
+//  BucketList
+//
+//  Created by Rodrigo on 16-08-24.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapKitView: View {
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    
+    var body: some View {
+        VStack {
+            Map(position: $position)
+                .mapStyle(.hybrid(elevation: .realistic))
+                .onMapCameraChange { context in
+                    print(context.region)
+                }
+            
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+                
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MapKitView()
+}
