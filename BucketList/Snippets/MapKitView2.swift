8cc080d54b8e0bd81e238d72b22fc0d2d8771f72
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-08-17 00:15:11 -0400

    feat: Add MapKitView2 snippet

---
+++ BucketList/Snippets/MapKitView2.swift
@@ -0,0 +1,47 @@
+//
+//  MapKitView2.swift
+//  BucketList
+//
+//  Created by Rodrigo on 17-08-24.
+//
+
+import MapKit
+import SwiftUI
+
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+}
+
+struct MapKitView2: View {
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
+    var body: some View {
+        VStack {
+            Map {
+                ForEach(locations) { location in
+                    //Marker with default behavior
+                    //Marker(location.name, coordinate: location.coordinate)
+                    
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Text(location.name)
+                            .font(.headline)
+                            .padding()
+                            .background(.blue.gradient)
+                            .foregroundStyle(.white)
+                            .clipShape(.capsule)
+                    }
+                    .annotationTitles(.hidden)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MapKitView2()
+}
