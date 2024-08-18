d43ea479eef47e7cf7ce566757c531f4a89d4412
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-08-18 13:53:09 -0400

    feat: Add editing annotations functionality

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
     )
     
     @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
     
     var body: some View {
         MapReader { proxy in
@@ -29,6 +30,9 @@ struct ContentView: View {
                             .frame(width: 44, height: 44)
                             .background(.white)
                             .clipShape(.circle)
+                            .onLongPressGesture {
+                                selectedPlace = location
+                            }
                     }
                 }
             }
@@ -37,6 +41,13 @@ struct ContentView: View {
                     let newLocation = Location(id: UUID(), name: "new location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
                     
                     locations.append(newLocation)
+                }
+            }
+            .sheet(item: $selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    if let index = locations.firstIndex(of: place) {
+                        locations[index] = newLocation
+                    }
                 }
             }
         }
