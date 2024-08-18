7ca7e7ec3025cb66dc4f7d62329c0ba784a3bad3
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-08-18 17:20:38 -0400

    feat: implement ViewModel of ContentView

---
+++ BucketList/ContentView-ViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by Rodrigo on 18-08-24.
+//
+
+import MapKit
+import Foundation
+
+
+extension ContentView {
+    @Observable
+    class ViewModel {
+        private(set) var locations: [Location]
+        var selectedPlace: Location?
+        
+        let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func addLocation(at point: CLLocationCoordinate2D) {
+            let newLocation = Location(id: UUID(), name: "new location", description: "", latitude: point.latitude, longitude: point.longitude)
+            locations.append(newLocation)
+            save()
+        }
+        
+        func updateLocation(location: Location) {
+            guard let selectedPlace else { return }
+            
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+                save()
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data")
+            }
+        }
+    }
+}
